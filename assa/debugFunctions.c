8d14a7007429ae41813132ffdb772907ddf3ac71
Matthias Klotz <matthias.klotz@student.tugraz.at>
2015-11-24 22:47:51 +0100

    created function prototypes (load, run, eval)

--- assa/debugFunctions.c
+++ assa/debugFunctions.c
@@ -14,3 +14,20 @@
 //          Johannes Kopf   <Matriculum Number>
 //-----------------------------------------------------------------------------
 //
+
+
+void load(char *filedirectory, char *program)
+{
+
+}
+
+void run(char *program, char **data, size_t *data_length,char **program_counter,
+        char **data_pointer, int steps, int *breakpoints)
+{
+
+}
+
+void eval(char **data, char **data_pointer, char *input_code)
+{
+  
+}
