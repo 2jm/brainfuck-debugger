9a318ebb1233322e3f41fbf230b80669b4d22cde
Matthias Klotz <matthias.klotz@student.tugraz.at>
2015-11-25 11:35:37 +0100

    put the fuctionheader(commentblocs) into the headerfile made some new includes (debugFunctions.h, interpreter.h)

--- assa/debugFunctions.c
+++ assa/debugFunctions.c
@@ -15,18 +15,10 @@
 //-----------------------------------------------------------------------------
 //
 
-#include <stdio.h>
+#include <debugFunctions.h>
+#include <interpreter.h>
 
 
-//-----------------------------------------------------------------------------
-///
-/// Loading the code character by character out of a file and write it into an 
-/// array.
-///
-/// @param *filedirectory directory of the codefile to fetch
-/// @param *program Array in which the bf-code gets written
-///
-//
 void load(char *filedirectory, char *program)
 {
   FILE *file;
@@ -45,38 +37,14 @@ void load(char *filedirectory, char *program)
   }
 }
 
-//-----------------------------------------------------------------------------
-///
-/// Executing the bf-code for a set number of steps (or till the code ends) by 
-/// calling the interpreter
-///
-/// @param *program Array in which the bf-code is saved
-/// @param **data   Datasegment
-/// @param *data_length length of the datasegment
-/// @param **programm_counter counter for position in the code where the
-///        interpreter should start
-/// @param **data_pointer points at the actual position in the datasegment
-/// @param *breakpoints array where the positions of the breakpoints are saved
-///
-//
+
 void run(char *program, char **data, size_t *data_length,char **program_counter,
         char **data_pointer, int *breakpoints)
 {
 
 }
 
-//-----------------------------------------------------------------------------
-///
-/// Manipulating the datasegment with a user entered bf-code
-///
-/// @param **data   Datasegment
-/// @param *data_length length of the datasegment
-/// @param *program Array in which the bf-code gets written
-/// @param **data_pointer points at the actual position in the datasegment
-/// @param *input_code the bf-code to execute, which was given with the
-///         funtionccall
-///
-//
+
 void eval(char **data, size_t *data_length, char **data_pointer, 
           char *input_code)
 {
