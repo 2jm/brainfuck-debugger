cc811d70a26bb77945745b31ebcabe6c72dcf1a2
Matthias Klotz <matthias.klotz@student.tugraz.at>
2015-12-01 14:07:05 +0100

    bf code is now checked in an own function also brackets are checked

--- assa/debugFunctions.c
+++ assa/debugFunctions.c
@@ -21,38 +21,49 @@
 void load (char *filedirectory, char *program)
 {
   FILE *file;
-  int character; // has to be a character to read EOF as -1
+  char *buffer;
+  int character; // has to be a integer to read EOF as -1
   int loopCounter = 0;
-  int sizeOfProgram;
+  int bracketCounter = 0;
+  int sizeOfProgram = 1024;
+  int *position;
+
+  *position = 0;
 
   if ((file = fopen (filedirectory, "r")) == NULL)
   {
-    printf ("Err at reading file\n");  //check if the file can be read
+    printf ("[ERR] reading the file failed\n");  //check if the file can be read
     //perror ("Error");
   }
   else
-  { //read the file char by char and write it into an array
+  { 
+    //read the file char by char and write it into an array
     while ((character = fgetc (file)) != EOF)
-    {
-      if (character == '<' || character == '>' || character == '+' ||
-          character == '-' || character == '.' || character == ',' ||
-          character == '[' || character == ']')
-      {
-        program[loopCounter] = character;
-        loopCounter++;
-      }
+    { 
+      //is 0 if #opened brackets == #closed brackets
+      bracketCounter = bracketCounter + check_code(program, character, position);
 
-      if(loopCounter == sizeOfProgram - 1)
+      //resizes the programmArray if the file is to long
+      if(*position == sizeOfProgram - 1)
       {
         program = realloc(program, sizeOfProgram * 2);
         sizeOfProgram = sizeOfProgram * 2;
       }
+
     }
-    program[loopCounter] = '\0';
+    program[loopCounter] = '\0';  //end of string
+
+    if(bracketCounter != 0)
+      pritntf("[ERR] parsing of input failed\n")
   }
+
   fclose (file);
 }
 
+
+/**********************
+RUN() WILL BE DELETED
+**********************/
 int run (char *program, char **data, size_t *data_length,
          char **program_counter, char **data_pointer, int *breakpoints)
 {
@@ -61,15 +72,15 @@ int run (char *program, char **data, size_t *data_length,
                       data_pointer, steps, breakpoints);
 }
 
+
 void eval (char **data, size_t *data_length, char **data_pointer,
            char *input_bfstring)
 {
-  //TODO: load befstring into code-variable (from main) when strlen(code) == 0
-  //TODO: (no program currently in memory)
-
+  int bracketCounter = 0;
+  int *position;
   int counter = 0;
-
   size_t len = strlen (input_bfstring);
+  
   // specification: maxlength of eval input: 80 chars
   if (len > 80)
   {
@@ -78,26 +89,39 @@ void eval (char **data, size_t *data_length, char **data_pointer,
 
   char new_bfcode[len];
   char *new_program_counter = new_bfcode; //pointer which points at the start
-  // of the new_bfcode array
-  int loopCounter;
+                                          // of the new_bfcode array
+  
+  int loopCounter;  //just for the for-loop
   for (loopCounter = 0; loopCounter < len; loopCounter++)
   {
-    if (input_bfstring[loopCounter] == '<'
-        || input_bfstring[loopCounter] == '>'
-        || input_bfstring[loopCounter] == '+'
-        || input_bfstring[loopCounter] == '-'
-        || input_bfstring[loopCounter] == '.'
-        || input_bfstring[loopCounter] == ','
-        || input_bfstring[loopCounter] == '['
-        || input_bfstring[loopCounter] == ']')
-    {
-      new_bfcode[counter] = input_bfstring[loopCounter];
-      counter++;
-    }
+    bracketCounter = 
+    bracketCounter + check_code(new_bfcode, input_bfstring[loopCounter], position);
   }
-  new_bfcode[counter] = '\0';
+  new_bfcode[counter] = '\0'; //end of string
 
-  interpreter (new_bfcode, data, data_length, &new_program_counter,
+  //if #opened brackets is not #closed brackets
+  if(bracketCounter != 0)
+    pritntf("[ERR] parsing of input failed\n");
+  else
+    interpreter (new_bfcode, data, data_length, &new_program_counter,
                data_pointer, 0, -1);
 }
 
+
+int check_code(char *programArray, int character,int *position)
+{
+  if (character == '<' || character == '>' || character == '+' ||
+      character == '-' || character == '.' || character == ',' ||
+      character == '[' || character == ']')
+  {
+    programArray[position] = character;
+    position++;
+  } 
+
+  if (character == '[')
+    return 1;
+  else if (character == ']')
+    return -1;
+  else
+    return 0;
+}
